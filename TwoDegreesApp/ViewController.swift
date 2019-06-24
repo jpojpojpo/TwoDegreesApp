e3708733595941bb67090ade60ca1e8777f98e1a
Henry Zhu <henry.zhu@henry.local>
2019-06-25 10:20:42 +1200

    Initial Commit

---
+++ TwoDegreesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwoDegreesApp
+//
+//  Created by Henry Zhu on 25/06/19.
+//  Copyright © 2019 Henry Zhu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TwoDegreesAppTests/TwoDegreesAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  TwoDegreesAppTests.swift
+//  TwoDegreesAppTests
+//
+//  Created by Henry Zhu on 25/06/19.
+//  Copyright © 2019 Henry Zhu. All rights reserved.
+//
+
+import XCTest
+@testable import TwoDegreesApp
+
+class TwoDegreesAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
